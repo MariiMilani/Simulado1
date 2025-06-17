4c4122caf23e8399c8baba61e17481f666d671ef
MariiMilani <159008870+MariiMilani@users.noreply.github.com>
2025-06-16 21:37:29 -0300

    Initial commit

---
+++ questoes/Q1.java
@@ -0,0 +1,26 @@
+//Data: 09/06/2025
+//Conteúdo: Avaliação prática
+
+package questoes;
+import utils.IO;
+
+public class Q1 {
+
+    public static void resolver(){
+        IO.imprimir("Avaliação - Questão 01 - ");
+
+        //Entrada
+        int n = IO.lerValorInteiro();
+
+        //Processamento
+        resolverQuadradoEAoCubo(n);
+        
+    }
+
+    public static void resolverQuadradoEAoCubo(int n){
+
+        // Sua solução aqui
+        
+    }
+
+}
